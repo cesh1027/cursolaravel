52c61d974a9449dc592b67a8bc68c3aa6eba9a58
cesh1027 <sanchezh.carlose@gmail.com>
2022-02-24 00:13:08 -0500

    se almacenan los datos ingresados al formulario

--- resources/views/trainers/create.blade.php
+++ resources/views/trainers/create.blade.php
@@ -2,31 +2,12 @@
 @extends('layouts.app')
 @section('title', 'Trainers Create')
 @section('content')
-    <div class="form-group">
-        <label for="">NOMBRE</label>
-        <input type="text" class="form-cotrol">
-    </div>
-
-    <button type="submit" class="btn btn-danger">Guardar</button>
-@endsection
-    
-
-{{-- <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</head>
-<body>
-    <div class="container">
+    <form class="form-group" method="POST" action="/trainers">
+        @csrf
         <div class="form-group">
-         <label for="">NOMBRE</label>
-         <input type="text" class="form-cotrol">
+            <label for="">NOMBRE</label>
+            <input type="text" name="name" class="form-cotrol">
         </div>
-
-        <button type="submit" class="btn btn-danger">Guardar</button>
-    </div>
-</body>
-</html> --}}
+    <button type="submit" class="btn btn-danger">Guardar</button>
+    </form>
+@endsection
